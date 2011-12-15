332f8d95469c3c054f05f821f3e311561279e130
Emmanuel Anne <manu@manu-home.dyndns.org>
2011-12-15 21:05:43 +0100

    add missing debian-raine directory
    add eventually a debian symlink to it to build the debian package for raine

---
+++ debian-raine/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the raine package
+#
+0 4	* * *	root	raine_maintenance
---
+++ debian-raine/raine-default.ex
@@ -0,0 +1,10 @@
+# Defaults for raine initscript
+# sourced by /etc/init.d/raine
+# installed at /etc/default/raine by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
