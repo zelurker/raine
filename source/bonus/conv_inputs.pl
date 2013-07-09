3b1ed2f7bdb496ae57e08e6a886f422795f382b5
Emmanuel Anne <emmanuel.anne@gmail.com>
2013-07-09 06:52:06 +0200

    conv_inputs: handle INP0, INP1, PORT_INCLUDE...

--- source/bonus/conv_inputs.pl
+++ source/bonus/conv_inputs.pl
@@ -8,6 +8,7 @@ my %raine_inputs =
      "IPT_COIN2a" => ["KB_DEF_COIN2", "MSG_COIN2"],
      "IPT_COIN3a" => ["KB_DEF_COIN3", "MSG_COIN3"],
      "IPT_COIN4a" => ["KB_DEF_COIN4", "MSG_COIN4"],
+     "IPT_UNKNOWN" => ["KB_DEF_UNKNOWN", "MSG_UNKNOWN"],
      "IPT_SERVICE1a" => ["KB_DEF_SERVICE", "MSG_SERVICE"],
      "IPT_SERVICE" => ["KB_DEF_SERVICE", "MSG_SERVICE"],
      "IPT_TILT" => ["KB_DEF_TILT","MSG_TILT"],
@@ -84,8 +85,8 @@ my %raine_inputs =
      );
 
 my %raine_bits =
-    ( "IP_ACTIVE_LOW" => "BIT_ACTIVE_0",
-      "IP_ACTIVE_HIGH" => "BIT_ACTIVE_1" );
+    ( "IP_ACTIVE_LOW" => "INP0",
+      "IP_ACTIVE_HIGH" => "INP1" );
 
 sub handle_bitx() {
   if (/PORT_BITX\( ?(.+) \)/) {
@@ -111,100 +112,158 @@ $port-=2;
 my $port0 = $port;
 my $started = undef;
 
+sub mychomp {
+	s/\r//;
+	chomp;
+}
+
+my %ports;
 open(F,"<$ARGV[0]") || die "Impossible to open $ARGV[0]";
 while (<F>) {
-  if (/INPUT_PORTS_START\( (.+?) \)/) {
-    my $name = $1;
-    $name = "_".$name if ($name =~ /^\d/);
-    my $nb = -1;
-    while (<F>) {
-      if (/PORT_START/) {
-	$nb++;
-	$port+=2;
-      }
-      handle_bitx();
-      if (/PORT_BIT\( *(.+?) ?\)/) {
-	my $whole_line = $_;
-	my $args = $1;
-	if ($started) {
-	  print "\n";
-	} else {
-	  print "static struct INPUT_INFO $name\_inputs\[\] =\n";
-	  print "{\n";
-	  $started = 1;
+	mychomp;
+	my $name_inp;
+	my $name_incl = undef;
+	if (/INPUT_PORTS_START\( (.+?) *\)/) {
+		my $name = $1;
+		my $nb = -1;
+		while (<F>) {
+			mychomp;
+			if (/PORT_START\((.+?)\)/) {
+				$nb++;
+				$port+=2;
+				print STDERR "adding ports $name:$1\n";
+				$ports{"$name:$1"} = $port;
+				die "debile\n" if (!defined($ports{"$name:$1"}));
+			} elsif (/PORT_MODIFY\( *(.+?) *\)/) {
+				if (defined($ports{"$name_incl:$1"})) {
+					$port = $ports{"$name_incl:$1"};
+				} else {
+					print STDERR "port_modify: unknown port $name_incl:$1\n";
+					exit(1);
+				}
+			}
+			handle_bitx();
+			if (/(PORT_BIT|PORT_SERVICE|PORT_INCLUDE)\( *(.+?) ?\)/) {
+				my $whole_line = $_;
+				my $args = $2;
+				my $func = $1;
+				if ($started) {
+					print "\n";
+				} else {
+					print "static struct INPUT_INFO input_$name\[\] =\n";
+					print "{\n";
+					$started = 1;
+				}
+				$nb++;
+				while ((my ($nb,$bit,$function) = split(/\, */,$args))) {
+					$function = "IPT_SERVICE1" if (!$function && $func eq "PORT_SERVICE");
+					$function =~ s/ +$//;
+					$function = 1 if ($func eq "PORT_INCLUDE");
+					if ($function =~ /\|/) {
+						my @parts = split(/[ \t]*\|[ \t]*/,$function);
+						$function = $parts[0];
+						if ($parts[$#parts] eq "IPF_PLAYER1") {
+							$function .= "a";
+						} elsif ($parts[$#parts] eq "IPF_PLAYER2" || $parts[$#parts] eq "IPF_COCKTAIL") {
+							$function .= "b";
+						} elsif ($parts[$#parts] eq "IPF_PLAYER3") {
+							$function .= "c";
+						} elsif ($parts[$#parts] eq "IPF_PLAYER4") {
+							$function .= "d";
+						} elsif ($parts[$#parts] =~ /^IPF_\dWAY$/ ) {
+							$function .= "a"; # Suppose it's player1 by default...
+						} else {
+							print STDERR "Unknown attribute $parts[$#parts] in ",join(" | ",@parts),"\n";
+							exit;
+						}
+					} elsif (/PORT_PLAYER\((\d)/) {
+						$function .= chr(ord('a')+$1-1);
+					}
+					if ($function =~ /\d$/) { # number at the end...
+						# player 1 by default.
+						$function .= "a";
+					}
+					if (/PORT_NAME\((.+?)\)/) {
+						$name_inp = $1;
+					}
+
+					if ($function eq "IPT_OTHER") {
+						if (/PORT_NAME\(\"(.+?)\"\)/) {
+							$function = $1;
+							if ($function =~ s/^P2/P1/) {
+								print "/* P2 Control */\n";
+							}
+						}
+					}
+
+					if ($function) {
+						if ($raine_inputs{$function}) {
+							my ($key,$input) = @{$raine_inputs{$function}};
+							if ($key) {
+								$bit = $raine_bits{$bit} || "INP1";
+								$input =~ s/MSG_//;
+								;
+								$nb = hex($nb) if ($nb =~ /^0x/);
+								if ($name_inp) {
+									print "  { $key, $name_inp, ";
+									if ($bit eq "INP1") {
+										$bit = "BIT_ACTIVE_1";
+									} else {
+										$bit = "BIT_ACTIVE_0";
+									}
+								} else {
+									print "  $bit( $input, ";
+								}
+								if ($nb > 255) {
+									print sprintf("0x%02x, ",$port+1), sprintf("0x%02x",$nb>>8);
+								} else {
+									print sprintf("0x%02x, ",$port), sprintf("0x%02x",$nb);
+								}
+								if ($name_inp) {
+									print ", $bit },";
+									$name_inp = undef;
+								} else {
+									print "),";
+								}
+								print " //$1" if (/\/\/(.+)/);
+								print "\n";
+							}
+						} elsif ($func eq "PORT_INCLUDE") {
+							$args =~ s/^ *//;
+							$args =~ s/ *$//;
+							$name_incl = $args;
+							print STDERR "port_include $name_incl\n";
+							$port = 0;
+							foreach (keys %ports) {
+								if (/^$name_incl\:(.*)/) {
+									$ports{"$name:$1"} = $ports{$_};
+									$port = $ports{$_} if ($ports{$_} > $port);
+								}
+							}
+							print "  INCL_INP( $args ),\n";
+						} elsif (!($function =~ /IPT_(UNKNOWN|UNUSED)/) && !($function =~ /IPT_SPECIAL/) && !($function =~ /IPT_SERVICE/)) {
+							print STDERR "Unknown function \'$function\'\n";
+						}
+					}
+					$_ = <F>;
+					mychomp;
+					handle_bitx();
+					if (/(PORT_BIT|PORT_SERVICE)\( *(.+?) ?\)/) {
+						$args = $2;
+						$func = $1;
+						next;
+					} else {
+						last;
+					}
+				} # while
+			} # if (/PORT_BIT/
+			last if (/PORTS_END/);
+		} # while (<F>)
+		print "  END_INPUT\n";
+		print "};\n\n";
+		$started = undef;
+		$port = $port0;
 	}
-	$nb++;
-	while ((my ($nb,$bit,$function) = split(/\, */,$args))) {
-	  $function =~ s/ +$//;
-	  if ($function =~ /\|/) {
-	    my @parts = split(/[ \t]*\|[ \t]*/,$function);
-	    $function = $parts[0];
-	    if ($parts[$#parts] eq "IPF_PLAYER1") {
-	      $function .= "a";
-	    } elsif ($parts[$#parts] eq "IPF_PLAYER2" || $parts[$#parts] eq "IPF_COCKTAIL") {
-	      $function .= "b";
-	    } elsif ($parts[$#parts] eq "IPF_PLAYER3") {
-	      $function .= "c";
-	    } elsif ($parts[$#parts] eq "IPF_PLAYER4") {
-	      $function .= "d";
-	    } elsif ($parts[$#parts] =~ /^IPF_\dWAY$/ ) {
-	      $function .= "a"; # Suppose it's player1 by default...
-	    } else {
-	      print STDERR "Unknown attribute $parts[$#parts] in ",join(" | ",@parts),"\n";
-	      exit;
-	    }
-	  } elsif (/PORT_PLAYER\((\d)/) {
-	    $function .= chr(ord('a')+$1-1);
-	    print STDERR "function $function\n";
-	  }
-	  if ($function =~ /\d$/) { # number at the end...
-	    # player 1 by default.
-	    $function .= "a";
-	  }
-
-	  if ($function eq "IPT_OTHER") {
-	    if (/PORT_NAME\(\"(.+?)\"\)/) {
-	      $function = $1;
-	      if ($function =~ s/^P2/P1/) {
-		print "/* P2 Control */\n";
-	      }
-	    }
-	  }
-
-	  if ($function) {
-	    if ($raine_inputs{$function}) {
-	      my ($key,$input) = @{$raine_inputs{$function}};
-	      if ($key) {
-		$bit = $raine_bits{$bit} || "BIT_ACTIVE_1"
-;
-		$nb = hex($nb) if ($nb =~ /^0x/);
-		if ($nb > 255) {
-		  print "  { $key, $input, 0x",sprintf("%02x",$port+1),", 0x",sprintf("%02x",$nb>>8),", $bit },\n";
-		} else {
-		  print "  { $key, $input, 0x",sprintf("%02x",$port),", 0x",sprintf("%02x",$nb),", $bit },\n";
-		}
-	      }
-	    } elsif (!($function =~ /IPT_(UNKNOWN|UNUSED)/) && !($function =~ /IPT_SPECIAL/) && !($function =~ /IPT_SERVICE/)) {
-	      print STDERR "Unknown function \'$function\'\n";
-	    }
-	  }
-	  $_ = <F>;
-	  handle_bitx();
-	  if (/PORT_BIT\( *(.+?) ?\)/) {
-	    $args = $1;
-	    next;
-	  } else {
-	    last;
-	  }
-	} # while
-      } # if (/PORT_BIT/
-      last if (/PORTS_END/);
-    } # while (<F>)
-    print "   { 0, NULL, 0, 0, 0 },\n";
-    print "};\n\n";
-    $started = undef;
-    $port = $port0;
-  }
 }
 close(F);
 
