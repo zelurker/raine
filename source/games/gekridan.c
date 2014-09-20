7f0d5df297623c3b4acaf9c548c107bff0109cd1
Emmanuel Anne <emmanuel.anne@gmail.com>
2014-09-20 13:53:45 +0200

    remove some old forgotten stuff

--- source/games/gekridan.c
+++ source/games/gekridan.c
@@ -300,11 +300,6 @@ static void load_gekirido(void)
 static void ClearGekirindan(void)
 {
    save_eeprom();
-
-   #ifdef RAINE_DEBUG
-      save_debug("ROM.bin",ROM,0x100000,0);
-      save_debug("RAM.bin",RAM,0x080000,0);
-   #endif
 }
 
 static void DrawGekirindan(void)
