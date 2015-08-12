e750f0f9ffe7f27cae4e9d612492911ff894335a
Emmanuel Anne <emmanuel.anne@gmail.com>
2015-08-12 11:03:17 +0200

    po.pl: add some comments, make it avoid a few files and...
    add load_progress and load_message functions

--- source/bonus/po.pl
+++ source/bonus/po.pl
@@ -2,10 +2,18 @@
 
 use strict;
 
+# Script to try to tag strings to translate in the sources
+# It's called with a list of files to process in parameter, like
+# po.pl `find source -name '*.c*'`
+# The problem is that it's not some kind of magic wand and it will tag too many
+# things by default, so some files must be restored manually after this, and
+# there are quite a few, so use this with cautious !
+
 my $file;
 my $func = 0;
 $| = 1;
 while ($file = shift @ARGV) {
+	next if ($file =~ /(alleg\/|confile.c|megasys1.c|psyforce.c|games\/tc\d|console\/)/);
 	open(F,"<$file") || die "can't open $file\n";
 	print "$file ... ";
 	open(G,">truc.cpp");
@@ -20,7 +28,7 @@ while ($file = shift @ARGV) {
 			}
 		}
 	   	if (!$func) {
-			if (/(print_ingame|print_menu_string|MessageBox|print_tf_state|add_layer_info) *\(/) { # specific function
+			if (/(print_ingame|print_menu_string|MessageBox|print_tf_state|add_layer_info|load_progress|load_message) *\(/) { # specific function
 				$func = 1;
 			}
 		}
@@ -30,10 +38,12 @@ while ($file = shift @ARGV) {
 			$rep -= s/(gettext|_)\("((.|\n)+?)"\)/"$2"/g;
 			$rep += s/"((.|\n)+?)"/_("$1")/g;
 			$rep -= s/_\("([^a-zA-Z]+?)"\)/"$1"/g; # remove no chars
+			$rep -= s/_\("(.+?\....)"\)/"$1"/g; # remove file with extension
 		} elsif ($func) {
 			$rep -= s/(gettext|_)\("((.|\n)+?)"\)/"$2"/g;
 			$rep += s/"((.|\n)+?)"/gettext("$1")/g;
 			$rep -= s/gettext\("([^a-zA-Z]+?)"\)/"$1"/g; # remove no chars
+			$rep -= s/gettext\("(.+?\....)"\)/"$1"/g; # remove file with extension
 		}
 		if (/;/) {
 			$active = 0;
