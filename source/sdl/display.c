a3cbdb67161b913e78b08a0e922a76547b344611
Emmanuel Anne <emmanuel.anne@gmail.com>
2016-05-19 13:03:06 +0200

    gui : windows doesn't need a bpp change anymore
    probably because of the fix about ScreenChange

--- source/sdl/display.c
+++ source/sdl/display.c
@@ -76,14 +76,7 @@ void adjust_gui_resolution() {
   }
   if (sdl_screen->flags & (SDL_DOUBLEBUF |SDL_HWSURFACE|SDL_OPENGL)) {
     print_debug("adjust_gui_res: disabling double buffer/opengl/doublebuffer\n");
-#ifdef RAINE_WIN32
-    // With the windib driver in fullscreen opengl, we can't restore the gui
-    // screen, it's just not updated. Changing the bpp forces a screen update
-    // This bug is windows specific, of course
-    int bpp = sdl_screen->format->BitsPerPixel < 32 ? 32 : 16;
-#else
     int bpp = sdl_screen->format->BitsPerPixel;
-#endif
     sdl_screen = SDL_SetVideoMode(sdl_screen->w,sdl_screen->h,bpp,(sdl_screen->flags | SDL_ANYFORMAT) & ~SDL_DOUBLEBUF & ~SDL_HWSURFACE & ~SDL_OPENGL);
   }
   SDL_ShowCursor(SDL_ENABLE);
