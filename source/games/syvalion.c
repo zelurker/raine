c457e9e7cb02618290f3b6cba3cdfc2340405c5e
Emmanuel Anne <emmanuel.anne@gmail.com>
2019-01-25 16:46:43 +0100

    syvalion update
    modern and much shorter rom loading, 8bpp not needed anymore without
    doing anything, use of external speed_hack, and fix region labels to
    something more useful !

--- source/games/syvalion.c
+++ source/games/syvalion.c
@@ -11,34 +11,53 @@
 #include "sasound.h"		// sample support routines
 #include "taitosnd.h"
 #include "timer.h"
-
+#include "speed_hack.h"
 
 static struct ROM_INFO rom_syvalion[] =
 {
-   {   "b51-01.bin", 0x00020000, 0x8dab004a, 0, 0, 0, },
-   {   "b51-02.bin", 0x00020000, 0x906ba440, 0, 0, 0, },
-   {   "b51-03.bin", 0x00020000, 0x8fd9b299, 0, 0, 0, },
-   {   "b51-04.bin", 0x00020000, 0x03aea658, 0, 0, 0, },
-   {   "b51-05.bin", 0x00020000, 0x47976ae9, 0, 0, 0, },
-   {   "b51-06.bin", 0x00020000, 0x81bef4f0, 0, 0, 0, },
-   {   "b51-07.bin", 0x00020000, 0x764d4dc8, 0, 0, 0, },
-   {   "b51-08.bin", 0x00020000, 0x9f6a535c, 0, 0, 0, },
-   {   "b51-09.bin", 0x00020000, 0xcbb4f33d, 0, 0, 0, },
-   {   "b51-10.bin", 0x00020000, 0x6aa97fbc, 0, 0, 0, },
-   {   "b51-11.bin", 0x00020000, 0xae9a9ac5, 0, 0, 0, },
-   {   "b51-12.bin", 0x00020000, 0x6b36d358, 0, 0, 0, },
-   {   "b51-13.bin", 0x00020000, 0xdab28958, 0, 0, 0, },
-   {   "b51-14.bin", 0x00020000, 0xdea7216e, 0, 0, 0, },
-   {   "b51-15.bin", 0x00020000, 0x30b2ee02, 0, 0, 0, },
-   {   "b51-16.bin", 0x00020000, 0xc0fcf7a5, 0, 0, 0, },
-   {   "b51-17.bin", 0x00080000, 0xd85096aa, 0, 0, 0, },
-   {   "b51-18.bin", 0x00080000, 0x8b23ac83, 0, 0, 0, },
-   {   "b51-19.bin", 0x00020000, 0x2abd762c, 0, 0, 0, },
-   {   "b51-20.bin", 0x00020000, 0x440b6418, 0, 0, 0, },
-   {   "b51-21.bin", 0x00020000, 0xaa111f30, 0, 0, 0, },
-   {   "b51-22.bin", 0x00020000, 0xe6c61079, 0, 0, 0, },
-   {   "b51-23.bin", 0x00010000, 0x734662de, 0, 0, 0, },
-   {           NULL,          0,          0, 0, 0, 0, },
+  { "b51-20.bin", 0x20000, 0x440b6418, REGION_CPU1, 0x00000, LOAD_8_16 },
+  { "b51-22.bin", 0x20000, 0xe6c61079, REGION_CPU1, 0x00001, LOAD_8_16 },
+  { "b51-19.bin", 0x20000, 0x2abd762c, REGION_CPU1, 0x40000, LOAD_8_16 },
+  { "b51-21.bin", 0x20000, 0xaa111f30, REGION_CPU1, 0x40001, LOAD_8_16 },
+  { "b51-23.bin", 0x10000, 0x734662de, REGION_ROM2, 0x00000, LOAD_NORMAL },
+  { "b51-16.bin", 0x20000, 0xc0fcf7a5, REGION_GFX1, 0x000000, LOAD_8_16 },
+  { "b51-12.bin", 0x20000, 0x6b36d358, REGION_GFX1, 0x000001, LOAD_8_16 },
+  { "b51-15.bin", 0x20000, 0x30b2ee02, REGION_GFX1, 0x040000, LOAD_8_16 },
+  { "b51-11.bin", 0x20000, 0xae9a9ac5, REGION_GFX1, 0x040001, LOAD_8_16 },
+  { "b51-08.bin", 0x20000, 0x9f6a535c, REGION_GFX1, 0x100000, LOAD_8_16 },
+  { "b51-04.bin", 0x20000, 0x03aea658, REGION_GFX1, 0x100001, LOAD_8_16 },
+  { "b51-07.bin", 0x20000, 0x764d4dc8, REGION_GFX1, 0x140000, LOAD_8_16 },
+  { "b51-03.bin", 0x20000, 0x8fd9b299, REGION_GFX1, 0x140001, LOAD_8_16 },
+  { "b51-14.bin", 0x20000, 0xdea7216e, REGION_GFX1, 0x200000, LOAD_8_16 },
+  { "b51-10.bin", 0x20000, 0x6aa97fbc, REGION_GFX1, 0x200001, LOAD_8_16 },
+  { "b51-13.bin", 0x20000, 0xdab28958, REGION_GFX1, 0x240000, LOAD_8_16 },
+  { "b51-09.bin", 0x20000, 0xcbb4f33d, REGION_GFX1, 0x240001, LOAD_8_16 },
+  { "b51-06.bin", 0x20000, 0x81bef4f0, REGION_GFX1, 0x300000, LOAD_8_16 },
+  { "b51-02.bin", 0x20000, 0x906ba440, REGION_GFX1, 0x300001, LOAD_8_16 },
+  { "b51-05.bin", 0x20000, 0x47976ae9, REGION_GFX1, 0x340000, LOAD_8_16 },
+  { "b51-01.bin", 0x20000, 0x8dab004a, REGION_GFX1, 0x340001, LOAD_8_16 },
+  { "b51-18.bin", 0x80000, 0x8b23ac83, REGION_SOUND2, 0x00000, LOAD_NORMAL }, // deltat
+  { "b51-17.bin", 0x80000, 0xd85096aa, REGION_SOUND1, 0x00000, LOAD_NORMAL }, // adpcm samples
+  { NULL, 0, 0, 0, 0, 0 }
+};
+
+static gfx_layout tilelayout =
+{
+	16,16,  /* 16x16 pixels */
+	32768,  /* 32768 tiles */
+	4,
+	{ 0, 1, 2, 3 },
+	{ 4, 0, 12, 8, 0x100000*8+4, 0x100000*8, 0x100000*8+12, 0x100000*8+8,
+		0x200000*8+4, 0x200000*8, 0x200000*8+12, 0x200000*8+8, 0x300000*8+4, 0x300000*8, 0x300000*8+12, 0x300000*8+8 },
+	{ 0*16, 1*16, 2*16, 3*16, 4*16, 5*16, 6*16, 7*16,
+		8*16, 9*16, 10*16, 11*16, 12*16, 13*16, 14*16, 15*16 },
+	16*16
+};
+
+static struct GFX_LIST syvalion_gfx[] =
+{
+   { REGION_GFX1, &tilelayout,  },
+   { 0,           NULL,             },
 };
 
 static struct INPUT_INFO input_syvalion[] =
@@ -103,8 +122,8 @@ static struct DSW_INFO dsw_syvalion[] =
 
 static struct ROMSW_DATA romsw_data_syvalion_0[] =
 {
-   { "Taito",                 0x00 },
-   { "Taito Japan",           0x02 },
+   { "Japan with copyright screen",                 0x00 },
+   { "Japan without copyright screen",           0x02 },
    { NULL,                    0    },
 };
 
@@ -120,136 +139,15 @@ static UINT8 *RAM_VIDEO;
 static UINT8 *RAM_COLOUR;
 static UINT8 *RAM_INPUT;
 
-static UINT8 *GFX_BG0;
-static UINT8 *GFX_BG0_SOLID;
-
 static UINT8 *GFX_FG0;
 
 static void load_syvalion(void)
 {
-   int ta,tb,tc;
-
    setup_z80_frame(CPU_Z80_0,CPU_FRAME_MHz(5,60));
 
-   if(!(RAM=AllocateMem(0x80000))) return;
-   if(!(ROM=AllocateMem(0x80000))) return;
-   if(!(GFX=AllocateMem(0x400000))) return;
-
-   GFX_BG0 = GFX+0x000000;
-
-   if(!load_rom("b51-16.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-15.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=0;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-14.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-13.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=8;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-12.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-11.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=2;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-10.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-09.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=10;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-08.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-07.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=4;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-06.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-05.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=12;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-04.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-03.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=6;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-   if(!load_rom("b51-02.bin", RAM+0x00000, 0x20000)) return;	// GFX
-   if(!load_rom("b51-01.bin", RAM+0x20000, 0x20000)) return;	// GFX
-   tb=14;
-   for(ta=0;ta<0x40000;ta+=16){
-      for(tc=0;tc<16;tc++){
-         GFX[tb+(tc<<4)]=RAM[ta+tc]&15;
-         GFX[tb+(tc<<4)+1]=RAM[ta+tc]>>4;
-      }
-      tb+=256;
-   }
-
-   if(!load_rom("b51-20.bin", RAM, 0x20000)) return;		// 68000 ROM
-   for(ta=0;ta<0x20000;ta++){
-      ROM[ta+ta]=RAM[ta];
-   }
-   if(!load_rom("b51-22.bin", RAM, 0x20000)) return;
-   for(ta=0;ta<0x20000;ta++){
-      ROM[ta+ta+1]=RAM[ta];
-   }
-   if(!load_rom("b51-19.bin", RAM, 0x20000)) return;
-   for(ta=0;ta<0x20000;ta++){
-      ROM[ta+ta+0x40000]=RAM[ta];
-   }
-   if(!load_rom("b51-21.bin", RAM, 0x20000)) return;
-   for(ta=0;ta<0x20000;ta++){
-      ROM[ta+ta+0x40001]=RAM[ta];
-   }
-
    RAMSize=0x48000+0x10000;
-
-   /*-----[Sound Setup]-----*/
-
-   Z80ROM=RAM+0x48000;
-   if(!load_rom("b51-23.bin", Z80ROM, 0x10000)) return;		// Z80 SOUND ROM
-
-   if(!(PCMROM=AllocateMem(0x100000))) return;
-   if(!load_rom("b51-18.bin",PCMROM+0x00000,0x80000)) return;	// ADPCM A rom
-   if(!load_rom("b51-17.bin",PCMROM+0x80000,0x80000)) return;	// ADPCM A rom
-   YM2610SetBuffers(PCMROM, PCMROM+0x80000, 0x80000, 0x80000);
-
+   if(!(RAM=AllocateMem(RAMSize))) return;
    AddTaitoYM2610(0x02EE, 0x028D, 0x10000);
-
-   /*-----------------------*/
 
    memset(RAM+0x00000,0x00,0x40000);
 
@@ -258,8 +156,6 @@ static void load_syvalion(void)
    RAM_COLOUR= RAM+0x31000+0x800;
    RAM_INPUT = RAM+0x32000;
 
-   GFX_BG0_SOLID = make_solid_mask_16x16(GFX_BG0, 0x4000);
-
    InitPaletteMap(RAM_COLOUR, 0x40, 0x10, 0x8000);
 
    set_colour_mapper(&col_map_xbbb_bbgg_gggr_rrrr);
@@ -267,16 +163,13 @@ static void load_syvalion(void)
    // 68000 Speed Hack
    // ----------------
 
-   WriteLong68k(&ROM[0x072A],0x13FC0000);	// move.b #$00,$AA0000
-   WriteLong68k(&ROM[0x072E],0x00AA0000);
-   WriteWord68k(&ROM[0x0732],0x6100-16);	// bra.s <loop>
+   ByteSwap(ROM,0x80000);
+   apply_rom_hack(ROM,0x72a,4);
 
    // Init tc0004vcu emulation
    // ------------------------
 
    tc0004vcu.RAM	= RAM_VIDEO;
-   tc0004vcu.GFX_BG0	= GFX_BG0;
-   tc0004vcu.GFX_BG0_MSK= GFX_BG0_SOLID;
    tc0004vcu.GFX_FG0	= GFX_FG0;
    tc0004vcu.tile_mask	= 0x3FFF;
    tc0004vcu.bmp_x	= 64;
@@ -299,51 +192,30 @@ static void load_syvalion(void)
  *  StarScream Stuff follows
  */
 
-   ByteSwap(ROM,0x80000);
-   ByteSwap(RAM,0x40000);
+   add_68000_rom(0,0x000000, 0x07FFFF, ROM+0x000000-0x000000);	// 68000 ROM
 
-   AddMemFetch(0x000000, 0x07FFFF, ROM+0x000000-0x000000);	// 68000 ROM
-   AddMemFetch(-1, -1, NULL);
-
-   AddReadByte(0x000000, 0x07FFFF, NULL, ROM+0x000000);			// 68000 ROM
-   AddReadByte(0x100000, 0x10FFFF, NULL, RAM+0x000000);			// 68000 RAM
-   AddReadByte(0x400000, 0x420FFF, NULL, RAM+0x010000);			// SCREEN RAM
-   AddReadByte(0x500000, 0x500FFF, NULL, RAM+0x031000);			// COLOR RAM
+   add_68000_ram(0,0x100000, 0x10FFFF, RAM+0x000000);			// 68000 RAM
+   AddReadBW(0x400000, 0x420FFF, NULL, RAM+0x010000);			// SCREEN RAM
+   add_68000_ram(0,0x500000, 0x500FFF, RAM+0x031000);			// COLOR RAM
    AddReadByte(0x200000, 0x200003, tc0220ioc_rb_port, NULL);		// INPUT
    AddReadByte(0x300000, 0x300003, tc0140syt_read_main_68k, NULL);	// SOUND COMM
-   AddReadByte(0x000000, 0xFFFFFF, DefBadReadByte, NULL);		// <Bad Reads>
-   AddReadByte(-1, -1, NULL, NULL);
 
-   AddReadWord(0x000000, 0x07FFFF, NULL, ROM+0x000000);			// 68000 ROM
-   AddReadWord(0x100000, 0x10FFFF, NULL, RAM+0x000000);			// 68000 RAM
-   AddReadWord(0x400000, 0x420FFF, NULL, RAM+0x010000);			// SCREEN RAM
-   AddReadWord(0x500000, 0x500FFF, NULL, RAM+0x031000);			// COLOR RAM
    AddReadWord(0x200000, 0x200003, tc0220ioc_rw_port, NULL);		// INPUT
    AddReadWord(0x110000, 0x11FFFF, NULL, RAM+0x000000);			// 68000 RAM [MIRROR]
-   AddReadWord(0x000000, 0xFFFFFF, DefBadReadWord, NULL);		// <Bad Reads>
-   AddReadWord(-1, -1, NULL, NULL);
 
-   AddWriteByte(0x100000, 0x10FFFF, NULL, RAM+0x000000);		// 68000 RAM
    AddWriteByte(0x410000, 0x410FFF, tc0004vcu_gfx_fg0_b_wb, NULL);	// FG0 GFX RAM
    AddWriteByte(0x401000, 0x420FFF, NULL, RAM+0x011000);		// SCREEN RAM
-   AddWriteByte(0x500000, 0x500FFF, NULL, RAM+0x031000);		// COLOR RAM
    AddWriteByte(0x400000, 0x400FFF, tc0004vcu_gfx_fg0_a_wb, NULL);	// FG0 GFX RAM
    AddWriteByte(0x200000, 0x200003, tc0220ioc_wb_port, NULL);		// INPUT
    AddWriteByte(0x300000, 0x300003, tc0140syt_write_main_68k, NULL);	// SOUND COMM
    AddWriteByte(0xAA0000, 0xAA0001, Stop68000, NULL);			// Trap Idle 68000
-   AddWriteByte(0x000000, 0xFFFFFF, DefBadWriteByte, NULL);		// <Bad Writes>
-   AddWriteByte(-1, -1, NULL, NULL);
 
-   AddWriteWord(0x100000, 0x10FFFF, NULL, RAM+0x000000);		// 68000 RAM
    AddWriteWord(0x410000, 0x410FFF, tc0004vcu_gfx_fg0_b_ww, NULL);	// FG0 GFX RAM
    AddWriteWord(0x401000, 0x420FFF, NULL, RAM+0x011000);		// SCREEN RAM
-   AddWriteWord(0x500000, 0x500FFF, NULL, RAM+0x031000);		// COLOR RAM
    AddWriteWord(0x400000, 0x400FFF, tc0004vcu_gfx_fg0_a_ww, NULL);	// FG0 GFX RAM
    AddWriteWord(0x200000, 0x200003, tc0220ioc_ww_port, NULL);		// INPUT
-   AddWriteWord(0x000000, 0xFFFFFF, DefBadWriteWord, NULL);		// <Bad Writes>
-   AddWriteWord(-1, -1, NULL, NULL);
 
-   AddInitMemory();	// Set Starscream mem pointers...
+   finish_conf_68000(0);
 
    GameMouse=1;
 }
@@ -391,10 +263,13 @@ static void DrawSyvalion(void)
 {
    ClearPaletteMap();
 
+   if (RefreshBuffers) {
+       tc0004vcu.GFX_BG0	= gfx[0];
+       tc0004vcu.GFX_BG0_MSK= gfx_solid[0];
+   }
+
    // Init tc0180vcu emulation
    // ------------------------
-
-   tc0004vcu_layer_count = 0;
 
    // BG0
    // ---
@@ -423,9 +298,10 @@ static struct VIDEO_INFO video_syvalion =
    512,
    416,
    64,
-   VIDEO_ROTATE_NORMAL|VIDEO_NEEDS_8BPP,
+   VIDEO_ROTATE_NORMAL,
+   syvalion_gfx
 };
-GMEI( syvalion, "Syvalion", TAITO, 1988, GAME_PLATFORM,
+GMEI( syvalion, "Syvalion", TAITO, 1988, GAME_SHOOT,
 	.romsw = romsw_syvalion,
 	.long_name_jpn = "サイバリオン",
 	.board = "B51",
