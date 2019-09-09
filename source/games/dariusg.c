7a71fb4dd5bd9dd4d2a772a6e3f546005590448b
Emmanuel Anne <emmanuel.anne@gmail.com>
2019-09-09 23:48:46 +0200

    dariusg turn, nothing new, but driver much smaller

--- source/games/dariusg.c
+++ source/games/dariusg.c
@@ -10,25 +10,25 @@
 #include "tc200obj.h"
 #include "savegame.h"
 #include "sasound.h"
-
+#include "blit.h" // clear_game_screen
 
 static struct ROM_INFO rom_dariusg[] =
 {
-   {   "d87-01.bin", 0x00200000, 0x3848a110, 0, 0, 0, },
-   {   "d87-02.bin", 0x00200000, 0x9250abae, 0, 0, 0, },
-   {   "d87-03.bin", 0x00200000, 0x4be1666e, 0, 0, 0, },
-   {   "d87-04.bin", 0x00200000, 0x2616002c, 0, 0, 0, },
-   {   "d87-05.bin", 0x00200000, 0x4e5891a9, 0, 0, 0, },
-   {   "d87-06.bin", 0x00200000, 0x3b97a07c, 0, 0, 0, },
-   {   "d87-08.bin", 0x00200000, 0x76d23602, 0, 0, 0, },
-   {   "d87-09.bin", 0x00080000, 0x6170382d, 0, 0, 0, },
-   {   "d87-10.bin", 0x00080000, 0x4149f66f, 0, 0, 0, },
-   {   "d87-11.bin", 0x00080000, 0xf7bed18e, 0, 0, 0, },
-   {   "d87-12.bin", 0x00080000, 0xde78f328, 0, 0, 0, },
-  LOAD8_16(  REGION_ROM2,  0x000000,  0x00040000,
-              "d87-13.bin",  0x15b1fff4,   "d87-14.bin",  0xeecda29a),
-   {   "d87-17.bin", 0x00200000, 0xe601d63e, 0, 0, 0, },
-   {           NULL,          0,          0, 0, 0, 0, },
+  { "d87-12.bin", 0x80000, 0xde78f328, REGION_CPU1, 0x000000, LOAD_8_32 },
+  { "d87-11.bin", 0x80000, 0xf7bed18e, REGION_CPU1, 0x000001, LOAD_8_32 },
+  { "d87-10.bin", 0x80000, 0x4149f66f, REGION_CPU1, 0x000002, LOAD_8_32 },
+  { "d87-16.bin", 0x80000, 0x8f7e5901, REGION_CPU1, 0x000003, LOAD_8_32 },
+  { "d87-03.bin", 0x200000, 0x4be1666e, REGION_GFX1 , 0x000000, LOAD_8_32S },
+  { "d87-04.bin", 0x200000, 0x2616002c, REGION_GFX1 , 0x000002, LOAD_8_32S },
+  { "d87-05.bin", 0x200000, 0x4e5891a9, REGION_GFX1 , 0x000000, LOAD_MASK4 },
+  { "d87-06.bin", 0x200000, 0x3b97a07c, REGION_GFX2 , 0x000000, LOAD_16_64S },
+  { "d87-17.bin", 0x200000, 0xe601d63e, REGION_GFX2 , 0x000004, LOAD_16_64S },
+  { "d87-08.bin", 0x200000, 0x76d23602, REGION_GFX2 , 0x000000, LOAD_MASK8 },
+  { "d87-13.bin", 0x40000, 0x15b1fff4, REGION_CPU2, 0x000000, LOAD_8_16 },
+  { "d87-14.bin", 0x40000, 0xeecda29a, REGION_CPU2, 0x000001, LOAD_8_16 },
+  { "d87-01.bin", 0x200000, 0x3848a110, REGION_SMP1 , 0x000000, LOAD_BE }, // C9 CA CB CC
+  { "d87-02.bin", 0x200000, 0x9250abae, REGION_SMP1 , 0x400000, LOAD_BE }, // CD CE CF D0
+  { NULL, 0, 0, 0, 0, 0 }
 };
 
 static struct ROMSW_DATA romsw_data_darius_gaiden_0[] =
@@ -48,21 +48,11 @@ static struct ROMSW_INFO romsw_dariusg[] =
 
 static struct ROM_INFO rom_dariusgx[] =
 {
-   {   "d87-01.bin", 0x00200000, 0x3848a110, 0, 0, 0, },
-   {   "d87-02.bin", 0x00200000, 0x9250abae, 0, 0, 0, },
-   {   "d87-03.bin", 0x00200000, 0x4be1666e, 0, 0, 0, },
-   {   "d87-04.bin", 0x00200000, 0x2616002c, 0, 0, 0, },
-   {   "d87-05.bin", 0x00200000, 0x4e5891a9, 0, 0, 0, },
-   {   "d87-06.bin", 0x00200000, 0x3b97a07c, 0, 0, 0, },
-   {   "d87-08.bin", 0x00200000, 0x76d23602, 0, 0, 0, },
-   { "dge_mpr0.bin", 0x00080000, 0xc5bd135c, 0, 0, 0, },
-   { "dge_mpr1.bin", 0x00080000, 0xbc030f6f, 0, 0, 0, },
-   { "dge_mpr2.bin", 0x00080000, 0x7be23e23, 0, 0, 0, },
-   { "dge_mpr3.bin", 0x00080000, 0x1c1e24a7, 0, 0, 0, },
-  LOAD8_16(  REGION_ROM2,  0x000000,  0x00040000,
-              "d87-13.bin",  0x15b1fff4,   "d87-14.bin",  0xeecda29a),
-   {   "d87-17.bin", 0x00200000, 0xe601d63e, 0, 0, 0, },
-   {           NULL,          0,          0, 0, 0, 0, },
+  { "dge_mpr3.bin", 0x80000, 0x1c1e24a7, REGION_CPU1, 0x000000, LOAD_8_32 },
+  { "dge_mpr2.bin", 0x80000, 0x7be23e23, REGION_CPU1, 0x000001, LOAD_8_32 },
+  { "dge_mpr1.bin", 0x80000, 0xbc030f6f, REGION_CPU1, 0x000002, LOAD_8_32 },
+  { "dge_mpr0.bin", 0x80000, 0xc5bd135c, REGION_CPU1, 0x000003, LOAD_8_32 },
+  { NULL, 0, 0, 0, 0, 0 }
 };
 
 static UINT8 *RAM_BG0;
@@ -91,79 +81,16 @@ static UINT8 *GFX_BG0_SOLID;
 static UINT8 *GFX_SPR;
 static UINT8 *GFX_SPR_SOLID;
 
-static void load_darius_gaiden(int version)
+static void load_dariusg()
 {
-   int ta,tb,tc;
-
    RAMSize=0x80000;
 
    if(!(RAM=AllocateMem(0x80000))) return;
-   if(!(ROM=AllocateMem(0x200000))) return;
-   if(!(GFX=AllocateMem(0x7EF000+0x7F3600))) return;
 
-   GFX_BG0 = GFX+0x7EF000;
-   GFX_SPR = GFX+0x000000;
-
-   tb=0;
-   if(!load_rom("d87-03.bin", ROM, 0x1FBC00)) return;	// 16x16 SPRITES ($7EF0)
-   for(ta=0;ta<0x1FBC00;ta++){
-      GFX[tb++]=ROM[ta]&15;
-      GFX[tb++]=ROM[ta]>>4;
-      tb+=2;
-   }
-   tb=2;
-   if(!load_rom("d87-04.bin", ROM, 0x1FBC00)) return;	// 16x16 SPRITES
-   for(ta=0;ta<0x1FBC00;ta++){
-      GFX[tb++]=ROM[ta]&15;
-      GFX[tb++]=ROM[ta]>>4;
-      tb+=2;
-   }
-   tb=0;
-   if(!load_rom("d87-05.bin", ROM, 0x1FBC00)) return;	// 16x16 SPRITES (MASK)
-   for(ta=0;ta<0x1FBC00;ta++){
-      tc=ROM[ta];
-      GFX[tb+3]|=((tc&0x40)>>6)<<4;
-      GFX[tb+2]|=((tc&0x10)>>4)<<4;
-      GFX[tb+1]|=((tc&0x04)>>2)<<4;
-      GFX[tb+0]|=((tc&0x01)>>0)<<4;
-      tb+=4;
-   }
-
-   tb=0;
-   if(!load_rom("d87-06.bin", ROM, 0x1FCD80)) return;	// 16x16 TILES ($7F36)
-   for(ta=0;ta<0x1FCD80;ta+=2){
-      GFX_BG0[tb++]=ROM[ta]&15;
-      GFX_BG0[tb++]=ROM[ta]>>4;
-      GFX_BG0[tb++]=ROM[ta+1]&15;
-      GFX_BG0[tb++]=ROM[ta+1]>>4;
-      tb+=4;
-   }
-   tb=4;
-   if(!load_rom("d87-17.bin", ROM, 0x1FCD80)) return;	// 16x16 TILES
-   for(ta=0;ta<0x1FCD80;ta+=2){
-      GFX_BG0[tb++]=ROM[ta]&15;
-      GFX_BG0[tb++]=ROM[ta]>>4;
-      GFX_BG0[tb++]=ROM[ta+1]&15;
-      GFX_BG0[tb++]=ROM[ta+1]>>4;
-      tb+=4;
-   }
-   tb=0;
-   if(!load_rom("d87-08.bin", ROM, 0x1FCD80)) return;	// 16x16 TILES (MASK)
-   for(ta=0;ta<0x1FCD80;ta+=2){
-      tc=ROM[ta];
-      GFX_BG0[tb+7]|=((tc&0x80)>>7)<<4;
-      GFX_BG0[tb+6]|=((tc&0x40)>>6)<<4;
-      GFX_BG0[tb+5]|=((tc&0x20)>>5)<<4;
-      GFX_BG0[tb+4]|=((tc&0x10)>>4)<<4;
-      GFX_BG0[tb+3]|=((tc&0x08)>>3)<<4;
-      GFX_BG0[tb+2]|=((tc&0x04)>>2)<<4;
-      GFX_BG0[tb+1]|=((tc&0x02)>>1)<<4;
-      GFX_BG0[tb+0]|=((tc&0x01)>>0)<<4;
-      tb+=8;
-   }
-
-   GFX_BG0_SOLID = MakeSolidTileMap16x16(GFX_BG0, 0x7F36);
-   GFX_SPR_SOLID = make_solid_mask_16x16(GFX_SPR, 0x7EF0);
+   GFX_SPR = load_region[REGION_GFX1];
+   GFX_BG0 = load_region[REGION_GFX2];
+   GFX_BG0_SOLID = make_solid_mask_16x16(GFX_BG0, 0x8000);
+   GFX_SPR_SOLID = make_solid_mask_16x16(GFX_SPR, 0x8000);
 
    // Setup 68020 Memory Map
    // ----------------------
@@ -190,30 +117,10 @@ static void load_darius_gaiden(int version)
    SCR1_YOFS=0xF400;
    SCR0_YOFS=0xFF80;
 
-   if(!load_rom_index(10, RAM, 0x80000)) return;
-   for(ta=0;ta<0x80000;ta++){
-      ROM[(ta<<2)+0]=RAM[ta];
-   }
-   if(!load_rom_index(9, RAM, 0x80000)) return;
-   for(ta=0;ta<0x80000;ta++){
-      ROM[(ta<<2)+1]=RAM[ta];
-   }
-   if(!load_rom_index(8, RAM, 0x80000)) return;
-   for(ta=0;ta<0x80000;ta++){
-      ROM[(ta<<2)+2]=RAM[ta];
-   }
-   if(!load_rom_index(7, RAM, 0x80000)) return;
-   for(ta=0;ta<0x80000;ta++){
-      ROM[(ta<<2)+3]=RAM[ta];
-   }
-
    // 68000 code
    M68000ROM = load_region[REGION_ROM2];
-   if(!(PCMROM=AllocateMem(0xa00000))) return;
-   load_be("d87-01.bin",PCMROM,0x200000);
-   load_be("d87-02.bin",PCMROM+0x400000,0x200000);
    max_banks_this_game=3; //=memory_region_length(REGION_SOUND1)/0x400000;
-   memset(PCMROM+0x800000,0,0x1fffff);
+   // memset(PCMROM+0x800000,0,0x1fffff);
 
    memset(RAM+0x00000,0x00,0x80000);
    memset(RAM+0x69000,0xFF,0x01000);
@@ -277,188 +184,23 @@ static void load_darius_gaiden(int version)
    setup_sound_68000();
 }
 
-static void load_dariusg() {
-  load_darius_gaiden(0);
-}
-
-static void load_dariusgx() {
-  load_darius_gaiden(1);
-}
-
 static void draw_darius_gaiden(void)
 {
-   int x16,y16,zz,zzz,zzzz;
-   int ta,x,y;
-   UINT8 *MAP;
-
    ClearPaletteMap();
 
-   if(check_layer_enabled(f3_bg0_id)){
-   MAKE_SCROLL_512x512_4_16(
-      (ReadWord68k(&RAM_SCR0[0])-SCR0_XOFS)>>6,
-      (ReadWord68k(&RAM_SCR0[8])-SCR0_YOFS)>>7
-   );
+   if(check_layer_enabled(f3_bg0_id))
+       draw_f3_opaque_layer((ReadWord68k(&RAM_SCR0[0])-SCR0_XOFS)>>6,(ReadWord68k(&RAM_SCR0[8])-SCR0_YOFS)>>7,RAM_BG0,GFX_BG0,0);
+   else
+       clear_game_screen(0);
 
-   zz=zzzz;
+   if(check_layer_enabled(f3_bg1_id))
+       draw_f3_layer((ReadWord68k(&RAM_SCR1[0])-SCR1_XOFS)>>6,(ReadWord68k(&RAM_SCR1[8])-SCR1_YOFS)>>7,RAM_BG1,GFX_BG0,GFX_BG0_SOLID);
 
-   for(y=(64-y16);y<(232+64);y+=16){
-   for(x=(64-x16);x<(320+64);x+=16){
+   if(check_layer_enabled(f3_bg2_id))
+       draw_f3_layer((ReadWord68k(&RAM_SCR2[0])-SCR2_XOFS)>>6,(ReadWord68k(&RAM_SCR2[8])-SCR2_YOFS)>>7,RAM_BG2,GFX_BG0,GFX_BG0_SOLID);
 
-      MAP_PALETTE_MAPPED_NEW(
-         ReadWord68k(&RAM_BG0[zz])&0x1FF,
-         32,
-         MAP
-      );
-
-      switch(RAM_BG0[zz]&0xC0){
-      case 0x00: Draw16x16_Mapped_Rot(&GFX_BG0[(ReadWord68k(&RAM_BG0[zz+2])&0x7FFF)<<8],x,y,MAP);        break;
-      case 0x40: Draw16x16_Mapped_FlipY_Rot(&GFX_BG0[(ReadWord68k(&RAM_BG0[zz+2])&0x7FFF)<<8],x,y,MAP);  break;
-      case 0x80: Draw16x16_Mapped_FlipX_Rot(&GFX_BG0[(ReadWord68k(&RAM_BG0[zz+2])&0x7FFF)<<8],x,y,MAP);  break;
-      case 0xC0: Draw16x16_Mapped_FlipXY_Rot(&GFX_BG0[(ReadWord68k(&RAM_BG0[zz+2])&0x7FFF)<<8],x,y,MAP); break;
-      }
-
-      zz=(zz&0xF80)|((zz+4)&0x7F);
-   }
-   zzzz=(zzzz+0x80)&0xFFF;
-   zz=zzzz;
-   }
-   }
-
-
-   if(check_layer_enabled(f3_bg1_id)){
-   MAKE_SCROLL_512x512_4_16(
-      (ReadWord68k(&RAM_SCR1[0])-SCR1_XOFS)>>6,
-      (ReadWord68k(&RAM_SCR1[8])-SCR1_YOFS)>>7
-   );
-
-   zz=zzzz;
-
-   for(y=(64-y16);y<(232+64);y+=16){
-   for(x=(64-x16);x<(320+64);x+=16){
-
-   ta=ReadWord68k(&RAM_BG1[zz+2])&0x7FFF;
-   if(ta!=0){
-
-      MAP_PALETTE_MAPPED_NEW(
-         ReadWord68k(&RAM_BG1[zz])&0x1FF,
-         32,
-         MAP
-      );
-
-      if(GFX_BG0_SOLID[ta]==0){
-         switch(RAM_BG1[zz]&0xC0){
-         case 0x00: Draw16x16_Trans_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Trans_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Trans_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Trans_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-      else{
-         switch(RAM_BG1[zz]&0xC0){
-         case 0x00: Draw16x16_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-   }
-
-      zz=(zz&0xF80)|((zz+4)&0x7F);
-   }
-   zzzz=(zzzz+0x80)&0xFFF;
-   zz=zzzz;
-   }
-   }
-
-   if(check_layer_enabled(f3_bg2_id)){
-   MAKE_SCROLL_512x512_4_16(
-      (ReadWord68k(&RAM_SCR2[0])-SCR2_XOFS)>>6,
-      (ReadWord68k(&RAM_SCR2[8])-SCR2_YOFS)>>7
-   );
-
-   zz=zzzz;
-
-   for(y=(64-y16);y<(232+64);y+=16){
-   for(x=(64-x16);x<(320+64);x+=16){
-
-   ta=ReadWord68k(&RAM_BG2[zz+2])&0x7FFF;
-   if(ta!=0){
-
-      MAP_PALETTE_MAPPED_NEW(
-         ReadWord68k(&RAM_BG2[zz])&0x1FF,
-         32,
-         MAP
-      );
-
-      if(GFX_BG0_SOLID[ta]==0){
-         switch(RAM_BG2[zz]&0xC0){
-         case 0x00: Draw16x16_Trans_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Trans_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Trans_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Trans_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-      else{
-         switch(RAM_BG2[zz]&0xC0){
-         case 0x00: Draw16x16_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-   }
-
-      zz=(zz&0xF80)|((zz+4)&0x7F);
-   }
-   zzzz=(zzzz+0x80)&0xFFF;
-   zz=zzzz;
-   }
-   }
-
-   if(check_layer_enabled(f3_bg3_id)){
-   MAKE_SCROLL_512x512_4_16(
-      (ReadWord68k(&RAM_SCR3[0])-SCR3_XOFS)>>6,
-      (ReadWord68k(&RAM_SCR3[8])-SCR3_YOFS)>>7
-   );
-
-   zz=zzzz;
-
-   for(y=(64-y16);y<(232+64);y+=16){
-   for(x=(64-x16);x<(320+64);x+=16){
-
-   ta=ReadWord68k(&RAM_BG3[zz+2])&0x7FFF;
-   if(ta!=0){
-
-      MAP_PALETTE_MAPPED_NEW(
-         ReadWord68k(&RAM_BG3[zz])&0x1FF,
-         32,
-         MAP
-      );
-
-      if(GFX_BG0_SOLID[ta]==0){
-         switch(RAM_BG3[zz]&0xC0){
-         case 0x00: Draw16x16_Trans_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Trans_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Trans_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Trans_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-      else{
-         switch(RAM_BG3[zz]&0xC0){
-         case 0x00: Draw16x16_Mapped_Rot(&GFX_BG0[ta<<8],x,y,MAP);        break;
-         case 0x40: Draw16x16_Mapped_FlipY_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0x80: Draw16x16_Mapped_FlipX_Rot(&GFX_BG0[ta<<8],x,y,MAP);  break;
-         case 0xC0: Draw16x16_Mapped_FlipXY_Rot(&GFX_BG0[ta<<8],x,y,MAP); break;
-         }
-      }
-   }
-
-      zz=(zz&0xF80)|((zz+4)&0x7F);
-   }
-   zzzz=(zzzz+0x80)&0xFFF;
-   zz=zzzz;
-   }
-   }
+   if(check_layer_enabled(f3_bg3_id))
+       draw_f3_layer((ReadWord68k(&RAM_SCR3[0])-SCR3_XOFS)>>6,(ReadWord68k(&RAM_SCR3[8])-SCR3_YOFS)>>7,RAM_BG3,GFX_BG0,GFX_BG0_SOLID);
 
    render_tc0200obj_mapped_f3system();
 
@@ -484,14 +226,14 @@ static struct DIR_INFO dir_dariusg[] =
    { "dariusgj", },
    { NULL, },
 };
-GAME( dariusg, "Darius Gaiden", TAITO, 1994, GAME_SHOOT,
-	.input = f3_system_inputs,
+#define input_dariusg f3_system_inputs
+#define dsw_dariusg NULL
+#define execute_dariusg ExecuteF3SystemFrame
+#define sound_dariusg f3_sound
+GME( dariusg, "Darius Gaiden", TAITO, 1994, GAME_SHOOT,
 	.romsw = romsw_dariusg,
-	.video = &video_dariusg,
-	.exec = ExecuteF3SystemFrame,
 	.long_name_jpn = "ダライアス外伝",
 	.board = "D87",
-	.sound = f3_sound,
 );
 static struct DIR_INFO dir_dariusgx[] =
 {
@@ -501,13 +243,10 @@ static struct DIR_INFO dir_dariusgx[] =
    { CLONEOF("dariusg"), },
    { NULL, },
 };
-GAME( dariusgx, "Darius Gaiden Extra", TAITO, 1994, GAME_SHOOT,
-	.input = f3_system_inputs,
+CLNE( dariusgx, dariusg, "Darius Gaiden Extra", TAITO, 1994, GAME_SHOOT,
 	.romsw = romsw_dariusg,
 	.video = &video_dariusg,
-	.exec = ExecuteF3SystemFrameB,
 	.long_name_jpn = "ダライアス外伝 Extra",
 	.board = "D87",
-	.sound = f3_sound,
 );
 
