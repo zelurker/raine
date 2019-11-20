4153add49e048320423a415304b416ee81cc2e49
Emmanuel Anne <emmanuel.anne@gmail.com>
2019-11-20 17:53:03 +0100

    pengo.c: fix sprites coordinates and reads out of rom
    sprites coordinates for the ghosts entering the tunels
    the reads out of rom are what made the mspac6m pills of level 2 to
    disappear !

--- source/games/pengo.c
+++ source/games/pengo.c
@@ -795,7 +795,7 @@ static void draw_pengo() {
 
       sx = 272 - spriteram_2[offs + 1]+16;
       sy = spriteram_2[offs] - 31+16;
-      if (sx >= 0 && sy >= 0 && sx <= 288 && sy <= 224) {
+      if (sx >= 0 && sy >= 0 && sx <= 288+16 && sy <= 224+16) {
 	UINT8 ta = spriteram[offs] >> 2;
 	UINT8 coul = (spriteram[offs+1] & 0x1f);
 	if (!coul) continue; // bank 0 is "all the colors transparent"
@@ -823,7 +823,7 @@ static void draw_pengo() {
 
       sx = 272 - spriteram_2[offs + 1]+16;
       sy = spriteram_2[offs] - 31+16;
-      if (sx >= 0 && sy >= 0 && sx <= 288 && sy <= 224) {
+      if (sx >= 0 && sy >= 0 && sx <= 288+16 && sy <= 224+16) {
 	UINT8 ta = spriteram[offs] >> 2;
 	UINT8 coul = (spriteram[offs+1] & 0x1f);
 	if (!coul) continue; // bank 0 is "all the colors transparent"
@@ -1052,7 +1052,7 @@ static void draw_emudx() {
 
       sx = (272 - spriteram_2[offs + 1])*2+16;
       sy = (spriteram_2[offs] - 31)*2+16;
-      if (sx >= 0 && sy >= 0 && sx <= 576 && sy <= 448) {
+      if (sx >= 0 && sy >= 0 && sx <= 576+32 && sy <= 448+32) {
 	UINT16 ta = spriteram[offs] >> 2;
 	UINT8 coul = (spriteram[offs+1] & 0xf);
 	if (!coul) continue; // bank 0 is "all the colors transparent"
@@ -1085,7 +1085,7 @@ static void draw_emudx() {
 
       sx = (272 - spriteram_2[offs + 1])*2+16;
       sy = (spriteram_2[offs] - 31)*2+16;
-      if (sx >= 0 && sy >= 0 && sx <= 576 && sy <= 448) {
+      if (sx >= 0 && sy >= 0 && sx <= 576+32 && sy <= 448+32) {
 	UINT16 ta = spriteram[offs] >> 2;
 	UINT8 coul = (spriteram[offs+1] & 0xf);
 	if (!coul) continue; // bank 0 is "all the colors transparent"
@@ -1196,7 +1196,7 @@ static struct ROM_INFO rom_mspacman[] =
   { "u5", 0x0800, 0xf45fbbcd, REGION_ROM1, 0x8000, LOAD_NORMAL },
   { "u6", 0x1000, 0xa90e7000, REGION_ROM1, 0x9000, LOAD_NORMAL },
   { "u7", 0x1000, 0xc82cd714, REGION_ROM1, 0xb000, LOAD_NORMAL },
-  FILL(0xc000, 0x3000,0, ROM1),
+  FILL(0xc000, 0x3000,0xff, ROM1),
   { "5e", 0x1000, 0x5c281d01, REGION_GFX1, 0x0000, LOAD_NORMAL },
   { "5f", 0x1000, 0x615af909, REGION_GFX2, 0x0000, LOAD_NORMAL },
   { NULL, 0, 0, 0, 0, 0 }
@@ -1210,7 +1210,7 @@ static struct ROM_INFO rom_mspacmab[] =
   { "boot4", 0x1000, 0x165a9dd8, REGION_ROM1, 0x3000, LOAD_NORMAL },
   { "boot5", 0x1000, 0x8c3e6de6, REGION_ROM1, 0x8000, LOAD_NORMAL },
   { "boot6", 0x1000, 0x368cb165, REGION_ROM1, 0x9000, LOAD_NORMAL },
-  FILL(0xa000, 0x5000,0, ROM1),
+  FILL(0xa000, 0x5000,0xff, ROM1),
   { NULL, 0, 0, 0, 0, 0 }
 };
 
@@ -1222,7 +1222,8 @@ static struct ROM_INFO rom_mspac6m[] =
   { "4.cpu", 0x1000, 0xe21c81ff, REGION_ROM1, 0x3000, LOAD_NORMAL },
   { "52.cpu", 0x1000, 0x48453707, REGION_ROM1, 0x8000, LOAD_NORMAL },
   { "62.cpu", 0x1000, 0x2e961bc4, REGION_ROM1, 0x9000, LOAD_NORMAL },
-  FILL(0xa000, 0x5000,0, ROM1),
+
+  FILL(0xa000,0x5000,0xff,ROM1),
 
   { "7.cpu", 0x0800, 0x2850148a, REGION_GFX1, 0x0000, LOAD_NORMAL },
   { "9.cpu", 0x0800, 0x6c3c6ebb, REGION_GFX1, 0x0800, LOAD_NORMAL },
@@ -1243,7 +1244,7 @@ static struct ROM_INFO rom_mspacpls[] =
   { "boot4", 0x1000, 0x165a9dd8, REGION_ROM1, 0x3000, LOAD_NORMAL },
   { "mspacatk.5", 0x1000, 0xe6e06954, REGION_ROM1, 0x8000, LOAD_NORMAL },
   { "mspacatk.6", 0x1000, 0x3b5db308, REGION_ROM1, 0x9000, LOAD_NORMAL },
-  FILL(0xa000, 0x5000,0, ROM1),
+  FILL(0xa000, 0x5000,0xff, ROM1),
   { NULL, 0, 0, 0, 0, 0 }
 };
 
@@ -1937,7 +1938,7 @@ static struct ROM_INFO rom_pacman25[] =
   { "maps3.rom", 0x1000, 0xc626ea9c, REGION_ROM1, 0xa000, LOAD_NORMAL },
   // maps 18-25
   { "maps4.rom", 0x1000, 0xf5ba954d, REGION_ROM1, 0xb000, LOAD_NORMAL },
-  FILL(0xc000, 0x3000,0, ROM1),
+  FILL(0xc000, 0x3000,0xff, ROM1),
 
   { "pacman25.5e", 0x1000, 0x11d47ae1, REGION_GFX1, 0x0000, LOAD_NORMAL },
   { NULL, 0, 0, 0, 0, 0 }
