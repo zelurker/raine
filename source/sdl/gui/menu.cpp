aea1daa5645c2033926e23825b1ee6641f3c6fd2
Emmanuel Anne <emmanuel.anne@gmail.com>
2020-05-08 10:05:35 +0200

    fix stupid utf8 bug in windows
    that is remove utf8 entierly in windows : not supported, too stupid !

--- source/sdl/gui/menu.cpp
+++ source/sdl/gui/menu.cpp
@@ -457,8 +457,13 @@ void TMenu::draw_top_frame() {
   font->dimensions(title,&w_title,&h_title);
   boxColor(sdl_screen,0,0,sdl_screen->w,h_title-1,bg_frame);
   font->put_string(HMARGIN,0,get_emuname(),fg_frame,bg_frame);
+#ifndef RAINE_WIN32
   int is_utf = test_utf(title);
   font->set_utf(is_utf);
+#else
+  font->set_utf(0); // utf-8 *not* supported by windows !!!
+  // actually setlocale does even convert utf8 strings to latin1 even if the definition is in utf8 for that !
+#endif
   font->put_string(sdl_screen->w-w_title,0,title,fg_frame,bg_frame);
 }
 char* TMenu::get_bot_frame_text() {
