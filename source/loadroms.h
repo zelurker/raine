9f2f82a43575d1354027b0f006896e29dded27f4
Emmanuel Anne <emmanuel.anne@gmail.com>
2021-01-04 15:39:36 +0100

    oops, bad copy & paste

--- source/loadroms.h
+++ source/loadroms.h
@@ -140,9 +140,9 @@ enum region_flags
 #define LOAD8_16(region, name, offset, size, crc) \
   { name, size, crc, REGION_##region, offset, LOAD_8_16 }
 #define LOAD16_32(region, name, offset, size, crc) \
-  { name, size, crc, REGION_##region, offset, LOAD_8_16 }
+  { name, size, crc, REGION_##region, offset, LOAD_16_32 }
 #define LOAD_32_SWAP_16(region, name, offset, size, crc) \
-  { name, size, crc, REGION_##region, offset, LOAD_8_16 }
+  { name, size, crc, REGION_##region, offset, LOAD32_SWAP_16 }
 
 #define LOAD8_16S(region, offset, size, rom1, crc1, rom2, crc2) \
 { rom1, size, crc1, REGION_##region, offset, LOAD_8_16S }, \
