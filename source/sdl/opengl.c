45bbe93398b7dc62df074a58164811cd7d2a9a0f
Emmanuel Anne <emmanuel.anne@gmail.com>
2022-02-02 18:10:55 +0100

    sdl/opengl: fix compilation in windows

--- source/sdl/opengl.c
+++ source/sdl/opengl.c
@@ -1,6 +1,6 @@
 #define NOGDI // define this before including windows.h to avoid al_bitmap !
 #define GL_GLEXT_PROTOTYPES
-#include "compat_sdl.h"
+#include <SDL.h>
 #ifdef ANDROID
 #include <GLES/gl.h>
 #include <GLES/glext.h>
