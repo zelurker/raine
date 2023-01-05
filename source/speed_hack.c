e4b2b098e142c91a56f70855aa5ff7a96d6b4ac7
Emmanuel Anne <emmanuel.anne@gmail.com>
2023-01-05 20:11:31 +0100

    fix speed hack module
    it created a crash related to savegames in windows 64 bits but the code
    was bad anyway, shouldn't have used passed variables without a struct to
    AddSaveData

--- source/speed_hack.c
+++ source/speed_hack.c
@@ -4,21 +4,24 @@
 #include "savegame.h"
 
 #define NB_ADR 32
-static UINT32 adr[NB_ADR];
-static UINT16 wordval[NB_ADR];
-static UINT16 hackval[NB_ADR];
-// Actually this speed_hack variable must be saved with the others (AddSave...) so it must be static to be in the same zone
-// so we need 2 functions to access it from outside... !
-static int speed_hack = 0; // speed hack detection (see execute_xxx_frame)
-static int hack_length;
+// A struct is not super convenient to write, but since this is saved through a AddSaveData, it's mandatory !
+static struct {
+    UINT32 adr[NB_ADR];
+    UINT16 wordval[NB_ADR];
+    UINT16 hackval[NB_ADR];
+    // Actually this speed_hack variable must be saved with the others (AddSave...) so it must be static to be in the same zone
+    // so we need 2 functions to access it from outside... !
+    int speed_hack; // speed hack detection (see execute_xxx_frame)
+    int hack_length;
+} data;
 
 void set_speed_hack() {
     // Used by drivers which use this as part of their test
-    speed_hack = 1;
+    data.speed_hack = 1;
 }
 
 int get_speed_hack() {
-    return speed_hack;
+    return data.speed_hack;
 }
 
 // No cpu handling yet, it's always CPU_68K_0
@@ -37,17 +40,17 @@ static void myWriteWord(UINT32 adr, UINT16 val) {
 void undo_hack() {
   // Undo the speed hack when a game is reseted, or the rom check will fail
   // Sigh ! This is to avoid to have to find 80 rom check functions or so.
-  if (speed_hack) {
+  if (data.speed_hack) {
     int n;
-    for (n=0; n<hack_length; n++)
-      myWriteWord(adr[n],wordval[n]);
-    hack_length = 0;
-    speed_hack = 0; // Not found yet...
+    for (n=0; n<data.hack_length; n++)
+      myWriteWord(data.adr[n],data.wordval[n]);
+    data.hack_length = 0;
+    data.speed_hack = 0; // Not found yet...
   }
 }
 
 void pWriteWord(UINT32 myadr, UINT16 val) {
-    adr[hack_length] = myadr;
+    data.adr[data.hack_length] = myadr;
     UINT8 *ptr = get_userdata(CPU_68K_0,myadr);
     UINT32 start,end;
     UINT8 *ptr2 = get_code_range(CPU_68K_0,myadr,&start,&end);
@@ -55,31 +58,31 @@ void pWriteWord(UINT32 myadr, UINT16 val) {
 	ptr = ptr2;
     if (!ptr)
 	fatal_error("speed hack: can't get ptr for %x !",myadr);
-    wordval[hack_length] = ReadWord(&ptr[myadr]);
-    hackval[hack_length] = val;
-    hack_length++;
-    if (hack_length == NB_ADR) {
+    data.wordval[data.hack_length] = ReadWord(&ptr[myadr]);
+    data.hackval[data.hack_length] = val;
+    data.hack_length++;
+    if (data.hack_length == NB_ADR) {
 	printf("speed hack overloads NB_ADR\n");
-	hack_length--;
+	data.hack_length--;
     }
     WriteWord(&ptr[myadr],val);
 }
 
 static void restore_hack() {
-    if (speed_hack) {
+    if (data.speed_hack) {
 	// This is for some extreme case where the speed hack is removed after being installed (cps2 rasters !)
 	// in this case when loading a savegame where it was installed, it needs to be reinstalled on load, that's what this function does
-	for (int n=0; n<hack_length; n++) {
-	    myWriteWord(adr[n],hackval[n]);
+	for (int n=0; n<data.hack_length; n++) {
+	    myWriteWord(data.adr[n],data.hackval[n]);
 	}
-	printf("restoration speed hack length %d\n",hack_length);
+	printf("restoration speed hack length %d\n",data.hack_length);
     }
 }
 
 void apply_hack(UINT32 pc, int kind) {
   print_ingame(180,gettext("Applied speed hack %d at %x"),kind,pc);
   print_debug("Applied speed hack %d at %x\n",kind,pc);
-  speed_hack = 1;
+  data.speed_hack = 1;
 
   pWriteWord(pc,0x4239);
   pWriteWord(pc+2,0xaa);
@@ -105,11 +108,11 @@ void apply_hack(UINT32 pc, int kind) {
     pWriteWord(pc+8,0x4e71);
     pWriteWord(pc+10,0x4e71);
   }
-  AddSaveData_ext("speedhack",(UINT8*)adr,((UINT8*)&hack_length)-((UINT8*)&adr[0])+sizeof(int));
+  AddSaveData_ext("speedhack",(UINT8*)&data,sizeof(data));
   AddLoadCallback(&restore_hack);
 }
 
 UINT32 get_speed_hack_adr(int n) {
-  return adr[n];
+  return data.adr[n];
 }
 
