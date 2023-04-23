9d19f5e45a64ceade9c1dfee823e55d33866e107
Emmanuel Anne <emmanuel.anne@gmail.com>
2023-04-23 20:23:14 +0200

    perl script to convert old scripts to lua
    syntax: perl source/bonus/to_lua.pl old_script > new_script

---
+++ source/bonus/to_lua.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use common::sense;
+
+while (<>) {
+	chomp;
+	s/^script/luascript/;
+	s/\$/0x/g;
+	s/poke (.+?) "(.+)"/poke($1,"$2")/;
+	s/poke (.+?) '(.+)'/poke($1,"$2")/;
+	s/poke (.+?) (.+?) /poke($1,$2) /;
+	s/poke (.+?) (.+)/poke($1,$2)/;
+	s/if ([^#]+)/if $1 then/;
+	s/endif/end/;
+	s/elsif/elseif/;
+	s/\)(and|or)\(/\) $1 \(/g;
+	s/#([^ ])/-- $1/;
+	s/#$/ --/;
+	s/!=/\~=/g;
+	s/then--/then --/;
+	s/  then/ then/g;
+	s/rnd\(\)/math.random\(\)\*255/;
+
+	say;
+}
+
