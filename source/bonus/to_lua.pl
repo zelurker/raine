e86e968cc21c441b08f09577bb028b1ea2c65ce6
Emmanuel Anne <emmanuel.anne@gmail.com>
2023-05-06 18:38:49 +0200

    to_lua: convert for loop, and add [lua] at end of title

--- source/bonus/to_lua.pl
+++ source/bonus/to_lua.pl
@@ -4,7 +4,7 @@ use common::sense;
 
 while (<>) {
 	chomp;
-	s/^script/luascript/;
+	s/^script "(.+?)"/luascript "$1 [lua]"/;
 	s/\$/0x/g;
 	s/poke ([^ ]+?) "(.+)"/poke($1,"$2")/;
 	s/poke ([^ ]+?) '(.+)'/poke($1,"$2")/;
@@ -21,6 +21,14 @@ while (<>) {
 	s/then--/then --/;
 	s/  then/ then/g;
 	s/rnd\(\)/math.random\(\)\*255/;
+	if (/^([ \t]*)for (.+?)=(.+?) .+?(<=|>=)(.+?) .+?([\+\-].+?) (.+)/) { # notice : <= & >= only !
+		my ($tab,$var,$beg,$end,$inc,$instr) = ($1,$2,$3,$5,$6,$7);
+		$inc =~ s/^\+//;
+		say $tab,"for $var=$beg,$end,$inc do";
+		say $tab,"   $instr";
+		say $tab,"end";
+		next;
+	}
 
 	say;
 }
