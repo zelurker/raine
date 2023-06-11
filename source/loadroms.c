cde833ceabcdcdc333f0f2ea7a27bdb752dfb7ad
Emmanuel Anne <emmanuel.anne@gmail.com>
2023-06-11 20:28:45 +0200

    take cheats from samsh5sp for the 2 new versions
    and fix a typo error for loadroms when using internet archive for
    samsh5pf

--- source/loadroms.c
+++ source/loadroms.c
@@ -1236,7 +1236,7 @@ beg:
 		   !strcmp(dir,"sstrikerk") || !strcmp(dir,"batsugun") || !strcmp(dir,"galaga") || !strcmp(dir,"kof2000ps2"))
 	       // they have the romset for 0.91.4, so there are a few exceptions...
 	       snprintf(url,512,"https://archive.org/download/efarcadeversionroms/Arcade Version Roms/FinalBurn v0.2.97.43 FullRoms.zip/%s.zip",dir);
-	   else if (!strcmp(dir,"samsh5sp") || !strcmp(dir,"samsh5fe")) // these 2 are in fbneo directory
+	   else if (!strcmp(dir,"samsh5pf") || !strcmp(dir,"samsh5fe")) // these 2 are in fbneo directory
 	       snprintf(url,512,"https://archive.org/download/fbnarcade-fullnonmerged/arcade/%s.zip",dir);
 	   else
 	       // There is a raine all roms directory for 0.91.4 !
