957e1cf3fbbc688d7a49c14db4060c2e1c64dc17
Emmanuel Anne <emmanuel.anne@gmail.com>
2024-06-08 02:32:04 +0200

    psikyosh: avoid a crash in soldivid
    because of a draw out of the screen limits.
    In linux the crash can be experienced only with a debug build with the
    sanitize option active, otherwise it goes totally unnoticed. Oh well,
    it's better to fix it anyway, and it's easy to do...

--- source/games/psikyosh.c
+++ source/games/psikyosh.c
@@ -1014,6 +1014,8 @@ static void psikyosh_drawgfxzoom(
 	    int ey = sy + sprite_screen_height;
 	    ex = MIN(ex, current_game->video->screen_x + current_game->video->border_size);
 	    ey = MIN(ey, current_game->video->screen_y + current_game->video->border_size);
+	    if (sy < 0) sy = 0;
+	    if (ey >= GameScreen.yfull) ey = GameScreen.yfull-1;
 
 	    int x_index_base;
 	    int y_index;
