8520c933c87f32a11c1a4cf9cc8bd0d9e751cdb8
Emmanuel Anne <emmanuel.anne@gmail.com>
2024-11-10 21:12:59 +0100

    finally revert to something shorter for fullscreen

--- source/sdl/dialogs/video_options.cpp
+++ source/sdl/dialogs/video_options.cpp
@@ -263,7 +263,7 @@ static menu_item_t video_items[] =
 #endif
 #endif
     // fullscreen from here is a nuisance, it's easier to handle from the keyboard handler
-{ _("Enable fullscreen mode (borderless window)"), &my_toggle_fullscreen, &display_cfg.fullscreen, 2, {0, 1}, {_("No"), _("Yes)"), _("Yes (real)")}},
+{ _("Fullscreen"), &my_toggle_fullscreen, &display_cfg.fullscreen, 2, {0, 1}, {_("No"), _("Yes") }},
 #ifdef RAINE_UNIX
 { _("Fullscreen hack for Intel"), NULL, &hack_fs, 2, {0, 1}, {_("No"),_("Yes")}},
 #endif
