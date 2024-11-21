9a0ce15a627244fad9816d85c7d669deadfced7a
Emmanuel Anne <emmanuel.anne@gmail.com>
2024-11-21 06:55:54 +0100

    fix smaller screen in fullscreen

--- source/sdl/gui.cpp
+++ source/sdl/gui.cpp
@@ -1175,6 +1175,10 @@ void StartGUI(void)
     if (display_cfg.screen_y + display_cfg.posy > desktop_h && !display_cfg.fullscreen) {
 	display_cfg.screen_y = desktop_h - display_cfg.screen_y;
     }
+    if (display_cfg.fullscreen) {
+	display_cfg.screen_x = desktop_w;
+	display_cfg.screen_y = desktop_h;
+    }
     resize(0,display_cfg.screen_x, display_cfg.screen_y);
     gui_end_hook = &gui_end;
     gui_start_hook = &gui_start;
