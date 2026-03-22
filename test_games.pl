be7a7927b708d166ad9f27a8a697f2f9492b3294
Emmanuel Anne <emmanuel.anne@gmail.com>
2026-03-22 03:11:56 +0100

    simple perl script to test all available games
    using the recent -gl-avail and -test command line options

---
+++ test_games.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use common::sense;
+
+open(F,"./raine -gl-avail|") || die "can't run raine -gl-avail";
+my $list = 0;
+while (<F>) {
+	chomp;
+	if (/^dfeveron/) {
+		$list = 1;
+		next;
+	}
+	if ($list) {
+		system("./raine -test $_");
+		if ($? == -1) {
+			die "can't execute raine -nogui -test $_";
+		} elsif ($? >> 8) {
+			say STDERR "$_ returned ",($? >> 8);
+		}
+	}
+}
+close(F);
+
